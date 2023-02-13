3a1f684afcf5473fdb36c6c2a18fbdebaea943ee
Haroldo22 <haroldzapien@gmail.com>
2023-02-13 11:36:20 -0600

    ejercicios de algoritmia

---
+++ Algoritmia/algoritmia.js
@@ -0,0 +1,28 @@
+function problema_1(){
+
+    var inputP1 = document.getElementById("inputP1").value;
+
+    var arr = inputP1.split(" ");
+
+    var arr2 = arr.reverse();
+
+    var arr3 = arr2.join(" ");
+
+    document.getElementById("p1-output").textContent = arr3;
+
+
+}
+
+
+function problema_3(){
+
+    var inputP3 = document.getElementById("inputP3").value;
+
+    var arr = inputP3.split(",");
+
+
+
+
+
+
+}
